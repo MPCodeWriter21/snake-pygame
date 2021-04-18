31b6145f966b1d6b7def25f6425b2a6f325bdb53
MPCodeWriter21 <CodeWriter21@Gmail.com>
2021-04-18 12:25:36 +0430

    New Features added(See README.md)

--- README.md
+++ README.md
@@ -17,21 +17,8 @@ Download the source code from the repository and run the file just as any other 
 python3 Snake\ Game.py
 ```
 
-The `difficulty` variable can be changed with the values provided in the comment to set the difficulty level.
 
 The rest of the code is properly commented and self explanatory. Tweaks can be made to change the play style or visuals of the game.
-
-
-## Screenshots
-
-![1](https://user-images.githubusercontent.com/32998741/33873439-27f635b2-df45-11e7-8fc1-f7812f17447a.png)
-*Written using PyCharm*
-
-![2](https://user-images.githubusercontent.com/32998741/33873437-2780ed2a-df45-11e7-9776-b1f151fa4e02.png)
-*Active game screen*
-
-![3](https://user-images.githubusercontent.com/32998741/33873440-28647360-df45-11e7-8291-b82d5646352f.png)
-*Game over screen*
 
 
 ## Prerequisites
@@ -44,6 +31,20 @@ The rest of the code is properly commented and self explanatory. Tweaks can be m
 * **Rajat Dipta Biswas** - *Initial work*
 
 See also the list of [contributors](https://github.com/rajatdiptabiswas/snake-pygame/graphs/contributors) who participated in this project.
+
+### CodeWriter21
+
+Updated by Mehrad Pooryoussof.
+
+Telegram Channel: [@CodeWriter](https://t.me/CodeWriter21)
+
+Blog: [CodeWriter21.blogsky.com](http://CodeWriter21.blogsky.com)
+
+*Added Features*
++ Score Eating added.
++ Auto Difficulty feature added.
++ Pause Ability added(You can use p button or Pause button to pause the game).
++ Ability to go through windows added.
 
 ## Acknowledgements
 * [Pygame Documentations](https://www.pygame.org/docs/)
--- Snake Game.py
+++ Snake Game.py
@@ -5,14 +5,13 @@ Made with PyGame
 
 import pygame, sys, time, random
 
-
 # Difficulty settings
 # Easy      ->  10
 # Medium    ->  25
 # Hard      ->  40
 # Harder    ->  60
 # Impossible->  120
-difficulty = 25
+difficulty = 10
 
 # Window size
 frame_size_x = 720
@@ -28,11 +27,9 @@ if check_errors[1] > 0:
 else:
     print('[+] Game successfully initialised')
 
-
 # Initialise game window
 pygame.display.set_caption('Snake Eater')
 game_window = pygame.display.set_mode((frame_size_x, frame_size_y))
-
 
 # Colors (R, G, B)
 black = pygame.Color(0, 0, 0)
@@ -41,16 +38,14 @@ red = pygame.Color(255, 0, 0)
 green = pygame.Color(0, 255, 0)
 blue = pygame.Color(0, 0, 255)
 
-
 # FPS (frames per second) controller
 fps_controller = pygame.time.Clock()
 
-
 # Game variables
 snake_pos = [100, 50]
-snake_body = [[100, 50], [100-10, 50], [100-(2*10), 50]]
+snake_body = [[100, 50], [100 - 10, 50], [100 - (2 * 10), 50]]
 
-food_pos = [random.randrange(1, (frame_size_x//10)) * 10, random.randrange(1, (frame_size_y//10)) * 10]
+food_pos = [random.randrange(1, (frame_size_x // 10)) * 10, random.randrange(1, (frame_size_y // 10)) * 10]
 food_spawn = True
 
 direction = 'RIGHT'
@@ -64,7 +59,7 @@ def game_over():
     my_font = pygame.font.SysFont('times new roman', 90)
     game_over_surface = my_font.render('YOU DIED', True, red)
     game_over_rect = game_over_surface.get_rect()
-    game_over_rect.midtop = (frame_size_x/2, frame_size_y/4)
+    game_over_rect.midtop = (frame_size_x / 2, frame_size_y / 4)
     game_window.fill(black)
     game_window.blit(game_over_surface, game_over_rect)
     show_score(0, red, 'times', 20)
@@ -75,19 +70,54 @@ def game_over():
 
 
 # Score
-def show_score(choice, color, font, size):
+def show_score(color, font, size):
     score_font = pygame.font.SysFont(font, size)
     score_surface = score_font.render('Score : ' + str(score), True, color)
     score_rect = score_surface.get_rect()
-    if choice == 1:
-        score_rect.midtop = (frame_size_x/10, 15)
-    else:
-        score_rect.midtop = (frame_size_x/2, frame_size_y/1.25)
+    score_rect.midtop = (frame_size_x / 10, 15)
+    rect = pygame.Rect(score_rect[0] - 5, score_rect[1] - 5, score_rect[2] + 10, score_rect[3] + 10)
+    pygame.draw.rect(game_window, red, rect, width=4)
     game_window.blit(score_surface, score_rect)
-    # pygame.display.flip()
+    return rect
 
 
-# Main logic
+# Difficulty
+def show_difficulty(color, font, size):
+    difficulty_font = pygame.font.SysFont(font, size)
+    if difficulty < 25:
+        diff = 'Easy'
+    elif difficulty < 40:
+        diff = 'Medium'
+    elif difficulty < 60:
+        diff = 'Hard'
+    elif difficulty < 120:
+        diff = 'Harder'
+    else:
+        diff = 'Impossible'
+    difficulty_surface = difficulty_font.render('Difficulty : ' + diff, True, color)
+    difficulty_rect = difficulty_surface.get_rect()
+    difficulty_rect.midtop = (frame_size_x * 9 / 10 - difficulty_rect[3], 15)
+    game_window.blit(difficulty_surface, difficulty_rect)
+
+
+# Eating Score
+def show_eating_score(color, font, size):
+    score_font = pygame.font.SysFont(font, size)
+    score_surface = score_font.render('Stop EATING Score!!', True, color)
+    score_rect = score_surface.get_rect()
+    score_rect.midtop = (frame_size_x / 2, frame_size_y - 25)
+    game_window.blit(score_surface, score_rect)
+
+
+# Game paused
+def show_pause(color, font, size):
+    pause_font = pygame.font.SysFont(font, size)
+    pause_surface = pause_font.render('Paused', True, color)
+    pause_rect = pause_surface.get_rect()
+    pause_rect.midtop = (frame_size_x / 2, frame_size_y / 2 - pause_rect[3] / 2)
+    game_window.blit(pause_surface, pause_rect)
+
+
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -104,11 +134,17 @@ while True:
                 change_to = 'LEFT'
             if event.key == pygame.K_RIGHT or event.key == ord('d'):
                 change_to = 'RIGHT'
+            if event.key == pygame.K_PAUSE or event.key == ord('p'):
+                change_to = 'PAUSE'
+                show_pause(red, 'consolas', 30)
+                pygame.display.update()
             # Esc -> Create event to quit the game
             if event.key == pygame.K_ESCAPE:
                 pygame.event.post(pygame.event.Event(pygame.QUIT))
 
     # Making sure the snake cannot move in the opposite direction instantaneously
+    if change_to == 'PAUSE':
+        continue
     if change_to == 'UP' and direction != 'DOWN':
         direction = 'UP'
     if change_to == 'DOWN' and direction != 'UP':
@@ -136,11 +172,6 @@ while True:
     else:
         snake_body.pop()
 
-    # Spawning food on the screen
-    if not food_spawn:
-        food_pos = [random.randrange(1, (frame_size_x//10)) * 10, random.randrange(1, (frame_size_y//10)) * 10]
-    food_spawn = True
-
     # GFX
     game_window.fill(black)
     for pos in snake_body:
@@ -149,22 +180,53 @@ while True:
         # xy-coordinate -> .Rect(x, y, size_x, size_y)
         pygame.draw.rect(game_window, green, pygame.Rect(pos[0], pos[1], 10, 10))
 
-    # Snake food
-    pygame.draw.rect(game_window, white, pygame.Rect(food_pos[0], food_pos[1], 10, 10))
-
     # Game Over conditions
     # Getting out of bounds
-    if snake_pos[0] < 0 or snake_pos[0] > frame_size_x-10:
-        game_over()
-    if snake_pos[1] < 0 or snake_pos[1] > frame_size_y-10:
-        game_over()
+    if snake_pos[0] < 0:
+        snake_pos[0] = frame_size_x - 10
+    if snake_pos[0] > frame_size_x - 10:
+        snake_pos[0] = 0
+    if snake_pos[1] < 0:
+        snake_pos[1] = frame_size_y - 10
+    if snake_pos[1] > frame_size_y - 10:
+        snake_pos[1] = 0
+
     # Touching the snake body
     for block in snake_body[1:]:
         if snake_pos[0] == block[0] and snake_pos[1] == block[1]:
             game_over()
 
-    show_score(1, white, 'consolas', 20)
+    # Show score
+    rect = show_score(white, 'consolas', 16)
+    eating_score = False
+    # Decrease score if snake is in score box
+    if score > 0:
+        if snake_pos[0] - rect[0] in range(rect[2]) and snake_pos[1] - rect[1] in range(rect[3]):
+            eating_score = True
+            show_score(red, 'consolas', 16)
+            show_eating_score(red, 'consolas', 20)
+            score -= 1
+            snake_body.pop()
+
+    # Set difficulty
+    difficulty = score * 2.5 + 10
+
+    # Show difficulty
+    show_difficulty(white, 'consolas', 16)
+
+    # Spawning food on the screen
+    if not food_spawn:
+        food_pos = [random.randrange(1, frame_size_x // 10) * 10,
+                    random.randrange(1 + (rect[1] + rect[3]) // 10, frame_size_y // 10) * 10]
+        food_spawn = True
+
+    # Snake food
+    pygame.draw.rect(game_window, white, pygame.Rect(food_pos[0], food_pos[1], 10, 10))
+
     # Refresh game screen
     pygame.display.update()
     # Refresh rate
-    fps_controller.tick(difficulty)
+    if eating_score:
+        fps_controller.tick(10)
+    else:
+        fps_controller.tick(difficulty)
